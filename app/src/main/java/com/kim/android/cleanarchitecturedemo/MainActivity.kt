ec8f929ed8e5e764d1eb7be2b74f81cea04f226e
Anil.Kumar <anil.kumar@sew.ai>
2026-05-18 20:53:54 +0530

    use constraint layout and remove compose from activity

--- app/src/main/java/com/kim/android/cleanarchitecturedemo/MainActivity.kt
+++ app/src/main/java/com/kim/android/cleanarchitecturedemo/MainActivity.kt
@@ -17,7 +17,8 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent {
+        setContentView(R.layout.activity_main)
+       /* setContent {
             CleanArchitectureDemoTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
@@ -26,9 +27,10 @@ class MainActivity : ComponentActivity() {
                     )
                 }
             }
-        }
+        }*/
     }
 }
+/*
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
@@ -44,4 +46,4 @@ fun GreetingPreview() {
     CleanArchitectureDemoTheme {
         Greeting("Android")
     }
-}
+}*/
