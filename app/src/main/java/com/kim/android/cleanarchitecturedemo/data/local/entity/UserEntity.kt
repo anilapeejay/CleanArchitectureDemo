1f606d0e659657f1ddd540d1d9fc951f9ea87e90
Anil.Kumar <anil.kumar@sew.ai>
2026-05-20 01:35:58 +0530

    create table

--- app/src/main/java/com/kim/android/cleanarchitecturedemo/data/local/entity/UserEntity.kt
+++ app/src/main/java/com/kim/android/cleanarchitecturedemo/data/local/entity/UserEntity.kt
@@ -1,5 +1,7 @@
 package com.kim.android.cleanarchitecturedemo.data.local.entity
 
+import androidx.room.Entity
 import androidx.room.PrimaryKey
 
+@Entity (tableName = "users")
 data class UserEntity(@PrimaryKey val id: Int, val name: String, val email: String)
