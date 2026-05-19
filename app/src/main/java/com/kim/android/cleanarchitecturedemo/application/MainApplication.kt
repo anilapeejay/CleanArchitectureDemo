91818baee149091db97e83a9064f948c11147883
Anil.Kumar <anil.kumar@sew.ai>
2026-05-20 00:58:55 +0530

    Create Application Class

---
+++ app/src/main/java/com/kim/android/cleanarchitecturedemo/application/MainApplication.kt
@@ -0,0 +1,8 @@
+package com.kim.android.cleanarchitecturedemo.application
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MainApplication : Application() {
+}
